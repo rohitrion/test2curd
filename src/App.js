0bc41f4b4a403e20837f0ba98c80b7277a992c2f
roshni gajbhiye <rohitkamui@gmail.comm>
2023-06-14 13:01:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react'
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 function App() {
+
+  const [data, setdata] = useState()
+
+  const [list, setlist] = useState([])
+
+  const [show, setshow] = useState(false)
+  const [index, setindex] = useState()
+
+  function handleAdd() {
+    setlist([...list, data])
+    setdata("")
+  }
+
+  const handeldelete = (index) => {
+    const del = list.filter((item, i) => i !== index)
+    setlist(del)
+  }
+
+  function handleEdit(i) {
+
+    setdata(list[i])
+    setshow(true)
+    setindex(i)
+
+  }
+
+  function handleupdate(){
+    list.splice(index,1,data)
+    setlist([...list])
+    setdata("")
+    setshow(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <h1>create empolyee</h1>
+        <br/>
+        <div className='inputt'>
+        <input value={data} onChange={(e) => setdata(e.target.value)} placeholder='enter the name' />
+        {
+          !show ? <Button variant="success " onClick={handleAdd} className="mb-2 sm">Add</Button> :
+            <Button variant="primary" className='sm' onClick={handleupdate}> Update</Button>
+        }
+       </div>
+   
+      <div className='table' >
+        <Table size="sm" striped bordered hover>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th colSpan={2}> Name</th>
+
+              <th>Functions</th>
+
+            </tr>
+          </thead>
+          <tbody>
+            {
+              list.map((item, i) => (
+                <tr>
+                  <td>{i}</td>
+                  <td colSpan={2}>{item}</td>
+                  <td> <Button className='btn' variant="warning" onClick={() => handleEdit(i)} >Edit</Button>{' '}
+                    <Button variant="danger" onClick={() => handeldelete(i)} >Delete</Button>{' '}
+                  </td>
+                </tr>
+
+              ))
+            }
+          </tbody>
+
+        </Table>
+
+      </div>
+
     </div>
+
+
+
   );
 }
 
